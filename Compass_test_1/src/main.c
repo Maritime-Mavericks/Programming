d314e8a2f100d107e583555569b74c15be15bfd7
Hritik Roy Chowdhury <hritik.roy.chowdhury@cern.ch>
2023-11-03 12:53:12 +0100

    Edited all codes, updated codes

---
+++ Compass_test_1/src/main.c
@@ -0,0 +1,134 @@
+#define F_CPU 16000000UL
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+#include <usart.h>
+
+#include "i2cmaster.h"
+
+#define PI	3.14159265359	/* Define Pi value */
+#define Declination	0.0527/* Define declination of location from where measurement going to be done. e.g. here we have added declination from location Pune city, India. we can get it from http://www.magnetic-declination.com */
+
+int offsetX = 0;
+int offsetY = 0;
+
+void Magneto_init(){		/* Magneto initialize function */
+	i2c_start(0x3C);	/* Start and write SLA+W */
+	i2c_write(0x00);	/* Write memory location address */
+	i2c_write(0x70);	/* Configure register A as 8-average, 15 Hz default, normal measurement */
+	i2c_write(0xA0);	/* Configure register B for gain */
+	i2c_write(0x00);	/* Configure continuous measurement mode in mode register */
+	i2c_stop();		/* Stop I2C */
+}
+
+int Magneto_GetHeadingOffset(){
+	int x, y, z;
+	double Heading;
+	i2c_start_wait(0x3C);	/* Start and wait for acknowledgment */
+	i2c_write(0x03);	/* Write memory location address */
+	i2c_rep_start(0x3D);/* Generate repeat start condition with SLA+R */
+	/* Read 16 bit x,y,z value (2's complement form) */
+	x = (((int)i2c_readAck()<<8) | (int)i2c_readAck()) + offsetX;
+	z = (((int)i2c_readAck()<<8) | (int)i2c_readAck()) + offsetY;
+	y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	i2c_read(0); // Must to a NAK on the last read
+	i2c_stop();		/* Stop I2C */
+	Heading = atan2((double)y,(double)x) + Declination;
+	if (Heading>2*PI)	/* Due to declination check for >360 degree */
+		Heading = Heading - 2*PI;
+	if (Heading<0)		/* Check for sign */
+		Heading = Heading + 2*PI;
+	return (Heading* 180 / PI);/* Convert into angle and return */
+}
+
+int Magneto_GetHeading(){
+	int x, y, z;
+	double Heading;
+	i2c_start_wait(0x3C);	/* Start and wait for acknowledgment */
+	i2c_write(0x03);	/* Write memory location address */
+	i2c_rep_start(0x3D);/* Generate repeat start condition with SLA+R */
+	/* Read 16 bit x,y,z value (2's complement form) */
+	x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	z = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	i2c_read(0); // Must to a NAK on the last read
+	i2c_stop();		/* Stop I2C */
+	Heading = atan2((double)y,(double)x) + Declination;
+	if (Heading>2*PI)	/* Due to declination check for >360 degree */
+		Heading = Heading - 2*PI;
+	if (Heading<0)		/* Check for sign */
+		Heading = Heading + 2*PI;
+	return (Heading* 180 / PI);/* Convert into angle and return */
+}
+
+int Magneto_GetX(){
+	int x, y, z;
+	double Heading;
+	i2c_start_wait(0x3C);	/* Start and wait for acknowledgment */
+	i2c_write(0x03);	/* Write memory location address */
+	i2c_rep_start(0x3D);/* Generate repeat start condition with SLA+R */
+	/* Read 16 bit x,y,z value (2's complement form) */
+	x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	z = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	i2c_read(0); // Must to a NAK on the last read
+	i2c_stop();		/* Stop I2C */
+	return x;
+}
+
+int Magneto_GetY(){
+	int x, y, z;
+	double Heading;
+	i2c_start_wait(0x3C);	/* Start and wait for acknowledgment */
+	i2c_write(0x03);	/* Write memory location address */
+	i2c_rep_start(0x3D);/* Generate repeat start condition with SLA+R */
+	/* Read 16 bit x,y,z value (2's complement form) */
+	x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	z = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	i2c_read(0); // Must to a NAK on the last read
+	i2c_stop();		/* Stop I2C */
+	return y;
+}
+
+int Magneto_GetZ(){
+	int x, y, z;
+	double Heading;
+	i2c_start_wait(0x3C);	/* Start and wait for acknowledgment */
+	i2c_write(0x03);	/* Write memory location address */
+	i2c_rep_start(0x3D);/* Generate repeat start condition with SLA+R */
+	/* Read 16 bit x,y,z value (2's complement form) */
+	x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	z = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	y = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
+	i2c_read(0); // Must to a NAK on the last read
+	i2c_stop();		/* Stop I2C */
+	return z;
+}
+
+int main() {
+
+	uart_init();
+	io_redirect();
+
+	// Initialize I2C communication
+	i2c_init();
+	Magneto_init();	
+
+	while(1) {
+		int heading = Magneto_GetHeading();
+		int headingOffset = Magneto_GetHeadingOffset();
+
+		int x = Magneto_GetX();
+		int y = Magneto_GetY();
+		int z = Magneto_GetZ();
+		printf("%d, %d, %d\n", x, y, z);
+		//printf("Deg: %d     Off-Deg: %d\n", heading, headingOffset);
+
+		_delay_ms(200);
+	}
+
+	return 0;
+}
